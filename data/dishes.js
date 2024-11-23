c21e3874cdb05c72cc6d64f85acc657d69b66835
Ivan Borisov - Yomive <my.mail.for.questions@gmail.com>
2024-11-23 13:05:52 +0100

    fe

--- data/dishes.js
+++ data/dishes.js
@@ -1,28 +1,5 @@
 const dishesCollection = [
-    {
-        id: 'DSH001',
-        name: 'Eggs Benedict',
-        price: '8$',
-        descENG: 'Poached eggs with hollandaise sauce.',
-        descRUS: 'Яйца пашот с голландским соусом.',
-        descUKR: 'Яйця пашот з голландським соусом.',
-        descTUR: 'Poşe yumurta ve hollandez sosu.',
-        descSRB: 'Јаја поширана са холандским сосом.',
-        descKAZ: 'Пашот жұмыртқасы голланд тұздығымен.',
-        descARM: 'Հոլանդական սոուսով պոչված ձու:',
-        descBLR: 'Яйкі пашот з галандскім соусам.',
-        image: '../data/dishesImages/expDish.png',
-        isTop: true,
-        isOpen: true,
-    },
-    {
-        id: 'DS002',
-        name: 'Pancakes',
-        price: '5$',
-        image: '../data/dishesImages/expDish.png',
-        isTop: false,
-        isOpen: false,
-    },
+    
     {
         id: 'SN001',
         name: 'French Fries',
